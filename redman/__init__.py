46eb8057114d4a39bd991a6f52baeab06760bd83
oktntko <oktntko@gmail.com>
2021-06-28 02:33:20 +0900

    init

--- redman/__init__.py
+++ redman/__init__.py
@@ -1,9 +1,9 @@
 import json
-import logging
-import os
+import re
+import subprocess
 from urllib import error, parse, request
 
-from plumbum import FG, cli, colors, local
+from fire import Fire
 from texttable import Texttable
 
 REDMINE_URL = "http://localhost:13000"
@@ -12,59 +12,23 @@ REDMIEN_USER_NAME = ""
 REDMIEN_PASSWORD = ""
 
 
-class Redman(cli.Application):
-    PROGNAME = colors.green
-    DESCRIPTION = None
-    DESCRIPTION_MORE = None
-    VERSION = colors.blue | "1.0.2"
-    USAGE = None
-    COLOR_USAGE = None
-    COLOR_GROUPS = {"Meta-switches": colors.bold & colors.yellow}
-    CALL_MAIN_IF_NESTED_COMMAND = True
-    ALLOW_ABBREV = False
+class Redman(object):
+    """A simple calculator class."""
 
-    cheese = cli.Flag(["cheese"], help="cheese, please")
-    chives = cli.Flag(["chives"], help="chives, instead")
-    opts = cli.Flag("--ops", help=colors.magenta | "This is help")
+    def __init__(self) -> None:
+        # 設定ファイルのチェック
+        # 必要コマンドのチェック
+        # --config=~/.redconfig
+        # default: my-redmine
+        # my-redmine:
+        #   - url: "localhost:13000"
+        #   - api_key: "bb2fcbf8e06106f10e9dfe6bcf828b9fa8774884"
+        #   - user_name: "user_name"
+        #   - password: "passworde"
+        # --redmine-name=default
+        pass
 
-    @cli.switch(names="-loglevel", argtype=int)
-    def set_log_level(self, level: int) -> None:
-        """Sets the log-level of the logger"""
-        logging.root.setLevel(level)
-
-    # def main(self, *args) -> int:
-    #     super().main()
-    #     # projects
-    #     # issuses
-    #     # TODO: users
-    #     if args:
-    #         print("Unknown command {0!r}".format(args[0]))
-    #         return 1   # error exit code
-    #     if not self.nested_command:           # will be ``None`` if no sub-command follows
-    #         print("No command given")
-    #         return 1   # error exit code
-
-    #     # 設定ファイルのチェック
-    #     # 必要コマンドのチェック
-    #     # --config=~/.redconfig
-    #     # default: my-redmine
-    #     # my-redmine:
-    #     #   - url: "localhost:13000"
-    #     #   - api_key: "bb2fcbf8e06106f10e9dfe6bcf828b9fa8774884"
-    #     #   - user_name: "user_name"
-    #     #   - password: "passworde"
-    #     # --redmine-name=default
-    #     os.getenv("HOME")
-    #     fzf = local["fzf"]
-    #     echo = local["echo"]
-    #     (echo["hoge"] | fzf) & FG
-
-
-@Redman.subcommand("projects")                    # attach 'geet commit'
-class Projects(cli.Application):
-    """creates a new commit in the current branch"""
-
-    def main(self) -> int:
+    def projects(self) -> None:
         params = {
             "key": REDMIEN_API_KEI
         }
@@ -74,12 +38,16 @@ class Projects(cli.Application):
                 body = json.load(res)
         except error.HTTPError as err:
             print(err.code)
-            return 1
+            return
         except error.URLError as err:
             print(err.reason)
-            return 1
+            return
 
         projects = body.get("projects")
+        if len(projects) <= 0:
+            print("no project")
+            return
+
         rows = [[
             "NO", "ID", "NAME", "DESCRIPTION",
         ]]
@@ -94,23 +62,60 @@ class Projects(cli.Application):
         table.set_deco(Texttable.HEADER | Texttable.VLINES)
         table.add_rows(rows)
 
-        fzf = local["fzf"]
-        echo = local["echo"]
-        hoge = echo[table.draw()] | fzf["--header-lines=2"]
-        a = hoge & FG
-        print(a)
-        print(hoge)
-        return 0
+        _ = subprocess.run(
+            f"echo '{table.draw()}' | fzf --header-lines=2",
+            shell=True, check=False, stdout=subprocess.PIPE) \
+            .stdout.decode('utf-8').strip()
+        print(_)
+        return
 
+    def issues(self, project: str) -> None:
+        params = {
+            "key": REDMIEN_API_KEI,
+            "project": project,
+            "op[status_id]": "o"
+        }
+        req = request.Request(f"{REDMINE_URL}/issues.json?{parse.urlencode(params)}")
+        try:
+            with request.urlopen(req) as res:
+                body = json.load(res)
+        except error.HTTPError as err:
+            print(err.code)
+            return
+        except error.URLError as err:
+            print(err.reason)
+            return
 
-@Redman.subcommand("issues")                      # attach 'geet push'
-class Issues(cli.Application):
-    """pushes the current local branch to the remote one"""
+        issues = body.get("issues")
+        if len(issues) <= 0:
+            print("no issue")
+            return
 
-    def main(self) -> None:
-        print("doing the push...")
+        rows = [[
+            "TRACKER", "STATUS", "PRIORITY", "#NO SUBJECT", "DESCRIPTION"
+        ]]
+
+        rows.extend([[
+            issue.get("tracker").get("name"),
+            issue.get("status").get("name"),
+            issue.get("priority").get("name"),
+            f"#{issue.get('id')} {issue.get('subject')}",
+            issue.get("description"),
+        ] for issue in issues])
+
+        table = Texttable()
+        table.set_deco(Texttable.HEADER | Texttable.VLINES)
+        table.add_rows(rows)
+
+        _ = subprocess.run(
+            f"echo '{table.draw()}' | fzf --header-lines=2",
+            shell=True, check=False, stdout=subprocess.PIPE) \
+            .stdout.decode('utf-8').strip()
+        m = re.search(r'#.*\|', _)
+        if m:
+            b = m.group()[:-1].strip()
+            print(b)
 
 
 def main() -> None:
-    app = Redman()
-    app.run()
+    Fire(Redman)
